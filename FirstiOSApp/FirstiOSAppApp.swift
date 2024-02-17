d577af3af23b16b716331d0106cba7f29dec2723
Christian <sancheznguyen@icloud.com>
2024-02-16 20:08:03 -0600

    Initial Commit

---
+++ FirstiOSApp/FirstiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstiOSAppApp.swift
+//  FirstiOSApp
+//
+//  Created by Christian Nguyen on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
